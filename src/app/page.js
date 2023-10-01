37431891c6b254614ef34118fa7235a842d34f5a
Blessing Onuoha <blessingonuoha.dev@gmail.com>
2023-10-01 13:11:23 +0100

    Add files via upload
    next boilerplate

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold">Hello world!</h1>
+      <p className="text-2xl">Welcome to your new Tailwind CSS app!</p>
+    </main>
+  );
+}
